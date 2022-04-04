f33c19cbed51d37d37a81d69719758a5ddb3e64a
Denis Radkevich <dzianis.radkevich@bankifi.com>
2022-04-04 16:41:20 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useWindowDimensions, ScrollView } from "react-native";
+
+import HTML, { MixedStyleRecord } from "react-native-render-html";
+
+const tagStyles = {
+  div: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: "black",
+    marginTop: 40,
+  },
+  a: {
+    fontSize: 16,
+    lineHeight: 24,
+    textDecorationLine: "none" as const,
+  },
+} as unknown as MixedStyleRecord;
+
+const App = () => {
+  const { width } = useWindowDimensions();  
+
+  const htmlTest =
+    "<div>Some long text to reproduce this issue. Either long text or increase in `div` text size is required for the problem to be revealed. With `fontSize: 2`, the link will be clickable in voice over mode. <a href='https://img5.goodfon.ru/wallpaper/nbig/3/73/abstraktsiia-antisfera-vodovorot-krasok-kartinka-chernyi-fon.jpg'>and link with description.</a></div>";
+
+  const source = {
+    html: htmlTest,
+  };
+
+  const onPress = (_, href) => {
+    console.log(href);
+  };
+
+  const renderersProps = {
+    a: {
+      onPress,
+    },
+  };
+
+  return (
+    <ScrollView contentContainerStyle={{ paddingHorizontal: 16, backgroundColor: "white", flex: 1 }}>
+      <HTML
+        contentWidth={width}
+        renderersProps={renderersProps}
+        source={source}
+        tagsStyles={tagStyles}
+      />
+    </ScrollView>
+  );
+};
+
+export default App;
